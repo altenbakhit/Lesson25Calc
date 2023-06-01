819534c94b878f2768b2aaa63094520552ab83b8
Bakytzhan Kenzhebayev <bakhitzhankenzhebayev@gmail.com>
2023-06-01 07:25:02 +0600

    Good

---
+++ src/modules/badScript.js
@@ -0,0 +1,8 @@
+const sliderModule = () => {
+    const slider = 'best slider'
+
+    console.log(slider);
+    console.log('Hello i am webpack watch');
+}
+
+export default sliderModule
---
+++ src/modules/slider.js
@@ -0,0 +1,10 @@
+const sliderModule = () => {
+    const slider = 'my slider'
+
+    console.log(slider);
+    console.log(slider);
+}
+
+export default sliderModule
+
+
