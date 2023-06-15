b11381deefb2d3a6836860fe37937f035700ad31
Bakytzhan Kenzhebayev <bakhitzhankenzhebayev@gmail.com>
2023-06-15 06:23:08 +0600

    Урок№23

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('01 jule 2023')
 menu()
 modal()
 tabs()
+slider()
