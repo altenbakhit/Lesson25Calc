9f1bb9cfc63fe862824c6f2d539a45202f282274
Bakytzhan Kenzhebayev <bakhitzhankenzhebayev@gmail.com>
2023-06-22 03:54:42 +0600

    Ошибка с калькулятором исправлена

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import calc from './modules/calc'
 
 timer('01 jule 2023')
 menu()
 modal()
 tabs()
 slider()
+calc(100)
